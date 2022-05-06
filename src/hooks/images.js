afbad7f3ce86ae580161ec7fcfb326508475d8fb
baobaomi900901 <baobaomi900901@icloud.com>
2022-05-06 15:33:32 +0800

    修改图片路劲

--- src/hooks/images.js
+++ src/hooks/images.js
@@ -3,11 +3,11 @@ import imageTwo from "../assets/img/agile-ui.jpg";
 import imageThree from "../assets/img/HKT.png";
 import imageFour from "../assets/img/ui-lego.png";
 import imageFive from "../assets/img/iSlace.jpg";
-import imageSix from "../assets/img/Design-system.jpg";
+import imageSix from "../assets/img/Design-system.png";
 import imageSeven from "../assets/img/Template-market.jpg";
 import imageEight from "../assets/img/wordpress.jpg";
 import imageNine from "../assets/img/dataview.jpg";
-import imageTen from "../assets/img/10.jpg";
+// import imageTen from "../assets/img/10.jpg";
 
 // import homeBigImage from "../assets/img/luckphoto.jpg";
 
@@ -21,7 +21,7 @@ const images = {
   imageSeven,
   imageEight,
   imageNine,
-  imageTen
+  // imageTen
 };
 
 // images.imageTwo.setAttribute("src", 'https://mtwork.oss-cn-shenzhen.aliyuncs.com/blog-assets/assets/img/agile-ui.jpg');
