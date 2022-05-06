7bc5ff7dfbf36fa860883f32bf75913464eda7be
baobaomi900901 <baobaomi900901@icloud.com>
2022-05-06 15:01:53 +0800

    引用路劲

--- src/hooks/images.js
+++ src/hooks/images.js
@@ -1,27 +1,27 @@
-// import imageOne from "../assets/img/vodafone.jpg";
-// import imageTwo from "../assets/img/agile-ui.jpg";
-// import imageThree from "../assets/img/HKT.png";
-// import imageFour from "../assets/img/ui-lego.png";
-// import imageFive from "../assets/img/iSlace.jpg";
-// import imageSix from "../assets/img/Design-system.jpg";
-// import imageSeven from "../assets/img/Template-market.jpg";
-// import imageEight from "../assets/img/wordpress.jpg";
-// import imageNine from "../assets/img/dataview.jpg";
-// import imageTen from "../assets/img/10.jpg";
+import imageOne from "../assets/img/vodafone.jpg";
+import imageTwo from "../assets/img/agile-ui.jpg";
+import imageThree from "../assets/img/HKT.png";
+import imageFour from "../assets/img/ui-lego.png";
+import imageFive from "../assets/img/iSlace.jpg";
+import imageSix from "../assets/img/Design-system.jpg";
+import imageSeven from "../assets/img/Template-market.jpg";
+import imageEight from "../assets/img/wordpress.jpg";
+import imageNine from "../assets/img/dataview.jpg";
+import imageTen from "../assets/img/10.jpg";
 
 // import homeBigImage from "../assets/img/luckphoto.jpg";
 
 const images = {
-  imageOne: '@/assets/img/vodafone.jpg',
-  imageTwo: '@/assets/img/agile-ui.jpg',
-  imageThree: '~/assets/img/HKT.png',
-  imageFour: '../assets/img/ui-lego.png',
-  imageFive: '../assets/img/iSlace.jpg',
-  imageSix: '../assets/img/Design-system.jpg',
-  imageSeven: '../assets/img/Template-market.jpg',
-  imageEight: '../assets/img/wordpress.jpg',
-  imageNine: '../assets/img/dataview.jpg',
-  imageTen: '../assets/img/10.jpg',
+  imageOne,
+  imageTwo,
+  imageThree,
+  imageFour,
+  imageFive,
+  imageSix,
+  imageSeven,
+  imageEight,
+  imageNine,
+  imageTen,
 };
 
 // images.imageTwo.setAttribute("src", 'https://mtwork.oss-cn-shenzhen.aliyuncs.com/blog-assets/assets/img/agile-ui.jpg');
