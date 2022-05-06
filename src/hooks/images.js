0e592e01607c1cd4b23bf6972d4b330e33dd0702
baobaomi900901 <baobaomi900901@icloud.com>
2022-05-06 14:51:01 +0800

    本地文件路劲

--- src/hooks/images.js
+++ src/hooks/images.js
@@ -9,29 +9,28 @@
 // import imageNine from "../assets/img/dataview.jpg";
 // import imageTen from "../assets/img/10.jpg";
 
-import homeBigImage from "../assets/img/home-big-image.png";
+// import homeBigImage from "../assets/img/luckphoto.jpg";
 
 const images = {
-
-  imageOne: 'src/assets/img/vodafone.jpg',
-  imageTwo: 'src/assets/img/agile-ui.jpg',
-  imageThree: 'src/assets/img/HKT.png',
-  imageFour: 'src/assets/img/ui-lego.png',
-  imageFive: 'src/assets/img/iSlace.jpg',
-  imageSix: 'src/assets/img/Design-system.jpg',
-  imageSeven: 'src/assets/img/Template-market.jpg',
-  imageEight: 'src/assets/img/wordpress.jpg',
-  imageNine: 'src/assets/img/dataview.jpg',
-  imageTen: 'src/assets/img/10.jpg',
+  imageOne: '../assets/img/vodafone.jpg',
+  imageTwo: '../assets/img/agile-ui.jpg',
+  imageThree: '../assets/img/HKT.png',
+  imageFour: '../assets/img/ui-lego.png',
+  imageFive: '../assets/img/iSlace.jpg',
+  imageSix: '../assets/img/Design-system.jpg',
+  imageSeven: '../assets/img/Template-market.jpg',
+  imageEight: '../assets/img/wordpress.jpg',
+  imageNine: '../assets/img/dataview.jpg',
+  imageTen: '../assets/img/10.jpg',
 };
 
 // images.imageTwo.setAttribute("src", 'https://mtwork.oss-cn-shenzhen.aliyuncs.com/blog-assets/assets/img/agile-ui.jpg');
 // console.log('images.imageTwo :>> ', images.imageTwo);
-const bigImage = {
-  homeBigImage,
-};
+// const bigImage = {
+//   homeBigImage,
+// };
 
 // export default images;
 
-export { images, bigImage };
+export { images };
 // https://mtwork.oss-cn-shenzhen.aliyuncs.com/blog-assets/assets/img/home-big-image.png
