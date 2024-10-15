bf8dff0012c9eac14e02e61b936c64efb41cc7df
moby <baobaomi900901@icloud.com>
2024-10-15 20:50:40 +0800

    优化

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,36 +8,34 @@ module.exports = {
         "dark-blue": "#0042f6",
       },
       height: {
-        '192': '48rem'
-      }
+        192: "48rem",
+      },
     },
     fontFamily: {
-      "yk": "YouSheBiaoTiHei"
+      yk: "YouSheBiaoTiHei",
     },
     screens: {
-      'mobile': '375px',
+      mobile: "375px",
       // => @media (min-width: 375px) { ... }
 
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
 
-      '3xl': '1920px',
+      "3xl": "1920px",
       // => @media (min-width: 1536px) { ... }
-    }
+    },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
